ad5ce245cfeaedd790611b1779d1e5e009e7cbad
Vishal <vishalkannan923@gmail.com>
2022-06-03 19:45:31 +0530

    first try

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 const start_minutes = 3;
-let time = start_minutes*60;
+let time = start_minutes * 60;
 let minutes;
 let seconds;
 
 const countdown = document.getElementById("timer")
 
-let handle = setInterval(updateTimer,1000)
-function updateTimer(){
-     minutes = Math.floor(time/60);
-     seconds = time %60;
-    if(seconds===0 && minutes===0){
+let handle = setInterval(updateTimer, 1000)
+
+function updateTimer() {
+    minutes = Math.floor(time / 60);
+    seconds = time % 60;
+    if (seconds === 0 && minutes === 0) {
         clearInterval(handle)
         checkans(false)
     }
-    seconds=seconds<10 ?'0'+seconds :seconds;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
 
     countdown.innerHTML = `${minutes}:${seconds}`;
 
     time--;
 }
+
 let ran_arr = [];
 let buttonPressed = [];
 let selectedButton
@@ -26,7 +28,7 @@ let score = 0;
 const audio = new Audio();
 const audio1 = new Audio();
 audio.src = "audio.wav"
-audio1.src="gameover.mp3"
+audio1.src = "gameover.mp3"
 for (let i = 0; i < 16; i++) {
     selectedButton = document.querySelectorAll(".btn")[i];
     selectedButton.addEventListener('click', fun)
@@ -38,8 +40,8 @@ function fun(evt) {
     buttonPressed.push(chosenButton);
     console.log("clicked")
     console.log(buttonPressed);
-    let ans = equalArray(ran_arr,buttonPressed)
-    if(ran_arr.length===buttonPressed.length){
+    let ans = equalArray(ran_arr, buttonPressed)
+    if (ran_arr.length === buttonPressed.length) {
         checkans(ans)
     }
 
@@ -54,49 +56,51 @@ function random() {
 
 function tilegen() {
     let ran = random()
-    let i=0;
+    let i = 0;
     if (!ran_arr.includes(ran)) {
         ran_arr.push(ran)
         console.log(ran_arr)
-        myLoop(ran_arr,i)
+        myLoop(ran_arr, i)
     } else {
         tilegen()
     }
 }
 
-function myLoop(ran_arr,i){
-    console.log("i=",i)
-        setTimeout( ()=>{
-            let myId = document.getElementById(ran_arr[i]);
-            myId.classList.remove('new');
-            let elem = myId;
-            setTimeout(()=>{elem.classList.add('new');},500)
+function myLoop(ran_arr, i) {
+    console.log("i=", i)
+    setTimeout(() => {
+        let myId = document.getElementById(ran_arr[i]);
+        myId.classList.remove('new');
+        let elem = myId;
+        setTimeout(() => {
+            elem.classList.add('new');
+        }, 500)
 
-            i++;
-            if(i<ran_arr.length){
-                myLoop(ran_arr,i);
-            }
-        },1000)
-    }
+        i++;
+        if (i < ran_arr.length) {
+            myLoop(ran_arr, i);
+        }
+    }, 1000)
+}
 
 
-function equalArray(ran_arr,buttonPressed){
+function equalArray(ran_arr, buttonPressed) {
     let count = 0;
 
     for (let i = 0; i < ran_arr.length; i++) {
-        console.log("ran=",ran_arr,"button=",buttonPressed)
+        console.log("ran=", ran_arr, "button=", buttonPressed)
 
-        if(ran_arr[i] === parseInt(buttonPressed[i])){
-                count++;
+        if (ran_arr[i] === parseInt(buttonPressed[i])) {
+            count++;
         }
     }
     // console.log("count=",count,"buttonpressedlength",buttonPressed.length)
-    if(buttonPressed.length>count){
+    if (buttonPressed.length > count) {
         checkans(false)
     }
-    if(count===ran_arr.length){
+    if (count === ran_arr.length) {
 
-            return true;
+        return true;
 
     }
 }
@@ -104,22 +108,24 @@ function equalArray(ran_arr,buttonPressed){
 function checkans(result) {
 
 
-    if(!result){
+    if (!result) {
 
         audio1.play()
+        // const score = localStorage.getItem("score")
+        LocalStorage(score)
+
         alert("GAME OVER \nYOUR SCORE = " + score)
-    }
-    else{
-        if(ran_arr.length === buttonPressed.length){
+    } else {
+        if (ran_arr.length === buttonPressed.length) {
             score++;
-            if(score>36){
+            if (score > 36) {
                 clearInterval(handle);
-                score = score+minutes*60+seconds;
+                score = score + minutes * 60 + seconds;
                 alert("GAME OVER \nYOUR SCORE = " + score)
 
 
             }
-            while(buttonPressed.length){
+            while (buttonPressed.length) {
                 buttonPressed.pop()
             }
 
@@ -130,5 +136,25 @@ function checkans(result) {
 }
 
 tilegen()
+
+function LocalStorage() {
+    const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+    const Score = {
+        Score: score
+    };
+    highScores.push(Score);
+    console.log("pushed")
+    highScores.sort((a, b) => b.Score - a.Score)
+    highScores.splice(5)
+    console.log(highScores)
+    localStorage.setItem("highScores",JSON.stringify(highScores));
+    const highScoresList = document.getElementById('highScoresList');
+    // const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+    document.getElementById("finalScore").innerHTML="TOP 5 SCORES:"
+    highScoresList.innerHTML = highScores.map(
+        score => {
+            return `<li class="high-score">1)${score.Score}</li>`;
+        }).join("");
+}
 
 
